0c32a8b203af134a9fa4179def787b07a2b28677
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2017-09-22 17:40:22 +0200

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+jenkinsfile-syntax
+======
+Set syntax highlighting for the Jenkinsfile
+
+Thanks to
+------
+[https://github.com/timols/](https://github.com/timols/sublime-text-2-packages) for the syntax highlighting
+
+Contribution
+------
+Contributions are appreciated.
+
+License
+------
+MIT-licensed. See LICENSE.
---
+++ jenkinsfile-syntax.py
@@ -0,0 +1,13 @@
+import sublime
+import sublime_plugin
+
+
+class JenkinsfileSyntaxCommand(sublime_plugin.EventListener):
+    def on_load(self, view):
+        file = view.file_name().split('/')
+
+        filename = file[len(file) - 1]
+
+        if filename.lower() == 'jenkinsfile':
+            view.set_syntax_file('Packages/jenkinsfile-syntax/Groovy.tmLanguage')
+
