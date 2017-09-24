e0b2f282b4e457bbcaa69328b096d530f5c66db6
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2017-09-24 17:51:34 +0200

    check length to be sure it is possible to get the last index

--- jenkinsfile-syntax.py
+++ jenkinsfile-syntax.py
@@ -6,6 +6,6 @@ class JenkinsfileSyntax(sublime_plugin.EventListener):
     def on_load(self, view):
         filename = view.file_name().split('/')
 
-        if filename[-1].lower() == 'jenkinsfile':
+        if len(filename) and filename[-1].lower() == 'jenkinsfile':
             view.set_syntax_file('Packages/jenkinsfile-syntax/Groovy.tmLanguage')
 
