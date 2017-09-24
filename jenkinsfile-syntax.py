3a404e7e479914d213b093d4e84a37fc178a9685
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2017-09-24 17:45:01 +0200

    use a more pythonic approach to get the last element in a list

--- jenkinsfile-syntax.py
+++ jenkinsfile-syntax.py
@@ -4,10 +4,8 @@ import sublime_plugin
 
 class JenkinsfileSyntax(sublime_plugin.EventListener):
     def on_load(self, view):
-        file = view.file_name().split('/')
+        filename = view.file_name().split('/')
 
-        filename = file[len(file) - 1]
-
-        if filename.lower() == 'jenkinsfile':
+        if filename[-1].lower() == 'jenkinsfile':
             view.set_syntax_file('Packages/jenkinsfile-syntax/Groovy.tmLanguage')
 
