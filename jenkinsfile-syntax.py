8b1a7c76b0e9282ea38ab2728776b7715bbeed81
mpkerr <mpkerr@computer.org>
2018-03-07 01:46:53 -0500

    activate syntax on any file that begins with 'jenkinsfile' (#5)

--- jenkinsfile-syntax.py
+++ jenkinsfile-syntax.py
@@ -6,6 +6,5 @@ class JenkinsfileSyntax(sublime_plugin.EventListener):
     def on_load(self, view):
         filename = view.file_name().split('/')
 
-        if len(filename) and 'jenkinsfile' in filename[-1].lower():
+        if len(filename) and filename[-1].lower().startswith('jenkinsfile'):
             view.set_syntax_file('Packages/jenkinsfile-syntax/Groovy.tmLanguage')
-
